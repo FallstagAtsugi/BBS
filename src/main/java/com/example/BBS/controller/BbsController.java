51dc099ec61e448fdb2321adee21cfca9e77d7f0
kboss <atsugi.design@gmail.com>
2023-07-03 15:18:50 +0900

    html作成

--- src/main/java/com/example/BBS/controller/BbsController.java
+++ src/main/java/com/example/BBS/controller/BbsController.java
@@ -3,15 +3,21 @@ package com.example.BBS.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BbsController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")
                     String name, Model model){
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @RequestMapping(value = "/")
+    public String index(){
+        return "index";
     }
 }
